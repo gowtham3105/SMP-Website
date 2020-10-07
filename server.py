13827b2b851c493407b3f9ced38d7f7bbda900d1
gowtham3105 <66207607+gowtham3105@users.noreply.github.com>
2020-10-07 18:03:09 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+import json
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app)
+
+@app.route("/faq", methods=['GET'])
+def faq():
+    f = open('faqs.json', "r")
+    data = f.read()
+    data = json.loads(data)
+    print(data)
+    return jsonify(data)
+
+app.run(host="0.0.0.0",debug=True, port="8000")
