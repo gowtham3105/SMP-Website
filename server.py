30c67861c2c8d470b6ad731aa4ad470497ef7e4d
gowtham3105 <66207607+gowtham3105@users.noreply.github.com>
2020-10-09 00:51:31 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -1,15 +1,26 @@
 from flask import Flask, request, jsonify
 import json
 from flask_cors import CORS
+import pymongo
+
+
+client = pymongo.MongoClient(
+    "mongodb+srv://gowtham:KZzaH6w8xpszthCz@smp-iitdh-database.zhcds.gcp.mongodb.net/FAQs?retryWrites=true&w=majority")
+db = client['FAQs']
+
 
 app = Flask(__name__)
 cors = CORS(app)
 
 @app.route("/faq", methods=['GET'])
 def faq():
-    f = open('faqs.json', "r")
-    data = f.read()
-    data = json.loads(data)
+    data = []
+
+    for x in db['Questions'].find():
+        x['_id'] = str(x['_id'])
+        data.append(x)
+        
+    data = json.loads(str(data).replace("'", "\""))
     return jsonify(data)
 
 app.run(host="0.0.0.0",debug=True, port="8000")
